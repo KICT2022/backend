effff95874aa5a7b2a9d8746f0755bc82b094302
soeunyang1222 <soeun9753@naver.com>
2025-07-29 18:37:11 +0900

    first commit

---
+++ yakpt/src/test/java/com/I2/yakpt/YakptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.I2.yakpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
